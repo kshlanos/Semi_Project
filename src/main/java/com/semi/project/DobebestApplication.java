e89b6c5c3e18ef04a2e96b3b176d4b66c4bc0d2f
hi <hi@DESKTOP-F4BC13L>
2022-09-28 16:06:01 +0900

    first commit

---
+++ src/main/java/com/semi/project/DobebestApplication.java
@@ -0,0 +1,13 @@
+package com.semi.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DobebestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DobebestApplication.class, args);
+	}
+
+}
