c68c24a2125eff641842d853efb5353c9454a6d8
jeonglever <jeonglever@gmail.com>
2022-10-05 14:06:03 +0900

    중간 commit

--- src/main/java/com/semi/project/admin/controller/NoticeController.java
+++ src/main/java/com/semi/project/admin/controller/NoticeController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 
 @Controller
-public class noticeController {
+public class NoticeController {
 	
 	@GetMapping("noticeMain")
 	public String getNoticeMain() {
